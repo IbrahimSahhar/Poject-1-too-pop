314d2bb109b6dd5247f95b8e33b22129b9a4a7c4
Ibrahim <113946679+IbrahimSahhar@users.noreply.github.com>
2023-07-25 20:13:34 +0300

    delete readme file and clear some comments

--- main.js
+++ main.js
@@ -67,35 +67,6 @@ hiddenDetailsButton = (Button, number) => {
   Button.style.color = "#6F6C90";
 };
 
-/* Start Questions section */
- // Initialize Swiper
-/* var swiper = new Swiper(".mySwiper", {
-  slidesPerView: 1,
-  spaceBetween: 30,
-  loop: true,
-  pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
-  },
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-  },
-  breakpoints: {
-    640: {
-      slidesPerView: 2,
-      spaceBetween: 20,
-    },
-    768: {
-      slidesPerView: 3,
-      spaceBetween: 40,
-    },
-    1024: {
-      slidesPerView: 3,
-      spaceBetween: 50,
-    },
-  },
-});*/
 var swiper = new Swiper(".mySwiper", {
   slidesPerView: 1,
   grabCursor: true,
